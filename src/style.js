ff4e9aec09ade75e11bc6a9687a0530bd949d042
Estela Diaz <diazsanchez.estela@gmail.com>
2019-04-07 15:40:45 -0700

    remove emotion because it is not configurable in create-react-app

---
+++ src/style.js
@@ -0,0 +1,21 @@
+import { css } from '@emotion/core'
+
+export const gradientStyle = css`
+	background: green;
+	height: 50vh;
+`
+
+export const globalStyle = css`
+body {
+	margin: 0;
+	padding: 0;
+	font-family: ;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	}
+`
+
+export const textStyle = css`
+	background: red;
+	height: 50vh;
+`
